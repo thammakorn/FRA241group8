85e4b5cf511124f1dc4221bbcde4e9e5684aa910
Nitisak01 <jj_nitisak@hotmail.com>
2016-11-26 20:42:20 +0700

    Set Server Mongo DB

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+
+var db = mongojs('mongodb://admin:admin@ds111178.mlab.com:11178/fiboreserchroom',['user']);
+
+router.get('/user',function (req, res, next) {
+    db.user.find(function (err, user) {
+      if(err){
+        res.send(err)
+      }
+      res.json(user);
+    })
+})
+
+////////username
+router.get('/user/username/:username',function (req, res, next) {
+  db.user.find({ username: req.params.username } , function (err, user) {
+    if(err){
+      res.send(err)
+    }
+    res.json(user);
+  })
+})
+
+/////password
+router.get('/user/password/:password',function (req, res, next) {
+  var password = req.params.password;
+  db.user.find({ password: password } , function (err, user) {
+    if(err){
+      res.send(err)
+    }
+    res.json(user);
+  })
+})
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
