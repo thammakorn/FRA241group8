ffac6792ba4473a10c8cab2a506fc3b7c74233b9
Nitisak01 <jj_nitisak@hotmail.com>
2016-11-28 04:57:50 +0700

    change login type

--- client/app/page2/page2.component.ts
+++ client/app/page2/page2.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 // import  { Service } from '../service/service'
 // import  { Choice} from '../choice'
 export class Date {
@@ -32,6 +33,27 @@ export class Page2Component {
     //         choices.done = !choices.done;
     //     });
     // }
+    constructor(public _router: Router) {
+        var x = document.cookie.split(';');
+        var cookievalue;
+        var i = 0;
+        for (; i < x.length; i++) {
+            if (x[i].split('=')[0].trim() == 'sessionID') {
+                cookievalue = x[i].split('=')[1];
+                break;
+            }
+        }
+        if (cookievalue === undefined) {
+            this._router.navigate(['']);
+        } else {
+            var res = atob(cookievalue).split('??');
+            if (atob(res[0]) == 'student' && atob(res[1]) == 'student') {
+                //admin
+            } else {
+                this._router.navigate(['']);
+            }
+        }
+    }
     dateshow: Date = {
         time : '',
         date : '-',
