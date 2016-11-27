4ae0884ac23a5db30b42f580472a16205b7ceb83
champ <champnarak15@hotmail.com>
2016-11-28 06:47:38 +0700

    about

---
+++ client/app/formteacher/formt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'formt',
+    templateUrl: 'formt.component.html',
+    /*styleUrls: ['page2.component.css']*/
+
+})
+export class FormtComponent {
+
+}
